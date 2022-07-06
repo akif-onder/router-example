a5dcb504a6ffd7f1b1ad3f6cc822426a72580272
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-06 22:43:42 +0300

    useParams, useNavigate

--- src/pages/Instructors.jsx
+++ src/pages/Instructors.jsx
@@ -1,11 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import {useNavigate} from 'react-router-dom';
 
 const Instructors = () => {
+
+  const navigate = useNavigate();
 
   const [instructors, setInstructors] = useState([]);
 
   const getInstructors = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => setInstructors(data))
       .catch((err) => console.log(err));
@@ -21,7 +24,8 @@ const Instructors = () => {
         {instructors?.map((inst) => {
           const { id, name } = inst;
           return (
-            <div className="col-sm-12 col-md-6 col-lg-4" key={id}>
+            <div className="col-sm-12 col-md-6 col-lg-4" key={id}  onClick={()=>navigate(`/instructors/${id}`)}
+            style={{cursor: 'pointer'}}>
               <img
                 src={`https://avatars.dicebear.com/v2/avataaars/${id}.svg`}
                 alt=""
