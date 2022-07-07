78baaf64c4142d7e18e33f693ae353602660f4e9
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-07 21:24:21 +0300

    paths generated

--- rc12-13_router-example/src/App.js
+++ rc12-13_router-example/src/App.js
@@ -1,12 +1,13 @@
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Instructors from './pages/Instructors';
-import NotFound from './pages/NotFound';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import InstructorDetail from './pages/InstructorDetail';
-
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InstructorDetail from "./pages/InstructorDetail";
+import { Navigate } from "react-router-dom";
+import Paths from "./pages/Paths";
 function App() {
   return (
     <>
@@ -16,6 +17,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/instructors" element={<Instructors />} />
           <Route path="/instructors/:id" element={<InstructorDetail />} />
+          <Route path="/paths" element={<Paths />}>
+            
+          </Route> />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
